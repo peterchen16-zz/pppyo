c3bf2cd07ff6be788497657eca54eee3001db00b
peter <peterchen16@gmail.com>
2014-10-30 02:59:18 -0400

    added pnotify

--- onepage/js/index.js
+++ onepage/js/index.js
@@ -11,6 +11,22 @@ function start_project() {
 		dataType: 'json',
 		success: function(data, textStatus, xhr) {
 			console.log(data);
+			messagePnotify('Success', 'Thank you for you interest. You have started a project with PPPYO. We will get back to you shortly.', 'success');
+			$("#close_modal_btn").click();
 		},
 	});
 }
+
+function messagePnotify(title, text, type){
+	var myStack = {"dir1":"down", "dir2":"right", "push":"top"};
+			new PNotify({
+				title: title,
+				text: text,
+				type: type,
+				closer: true,
+				delay: 20000,
+				mouse_reset: false, 
+				addclass: "stack-custom",
+				stack: myStack
+			});
+		}
